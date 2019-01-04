19c86ad8e6d6779fdab9f026e077d5ad12683c1e
Russell Moore <RussellM@batc.local>
2019-01-04 14:11:04 -0700

    made some changes

--- projects/todo/todo.js
+++ projects/todo/todo.js
@@ -1,12 +1,12 @@
- // toggle done and not done, add listener to the parent so that new children will have desired functionality
- $("ul").on("click", "li", function () {
+// toggle done and not done, add listener to the parent so that new children will have desired functionality
+$("ul").on("click", "li", function () {
     $(this).toggleClass("done");
 });
 
 // delete todo item
 $("ul").on("click", "span", function (e) {
     // fade out an then remove item
-    $(this).parent().fadeOut(1000, function (event) {
+    $(this).parent().fadeOut(1000, function () {
         $(this).remove();
     });
     // stops event from propagating to higher parent elements
@@ -20,24 +20,11 @@ $("input[type='text']").keypress(function (e) {
         let todo = $(this).val();
         // make new li
         $("ul").append("<li><span><i class='fa fa-trash-alt'></i></span> " + todo + "</li>");
-        $(this).val("");    
-    }    
+        $(this).val("");
+    }
 });
 
 $(".fa-plus").click(function () {
     $("input[type='text']").slideToggle();
-    $(".fa-plus").toggleClass("turn")                   
+    $(".fa-plus").toggleClass("turn")
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
